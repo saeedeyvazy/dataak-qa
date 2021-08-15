a9079fe1d5d54f550a50719c4cdd523e20688140
saeed eyvazy <saeed.eyvazy@gmail.com>
2021-08-15 23:36:01 +0430

    Add User profile name to Header component :star:

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { PrimaryButton } from '../PrimaryButton'
 import PROFILE_IMAGE from '@images/ellipse.png'
+import BOTTOM_ARROW from '@images/polygon.png'
 
 export const Header: React.FC<Props> = ({ title }) => {
 	return (
 		<div className='container flex items-center  justify-between h-16'>
 			<div className='flex items-center gap-4'>
+				<img src={BOTTOM_ARROW} alt='arrow' />
+				<h3 className='text-qa-dark-black font-bold text-sm'>مرضیه ابراهیمی</h3>
 				<img src={PROFILE_IMAGE} alt='profile' />
 				<PrimaryButton text='سوال جدید' />
 			</div>
