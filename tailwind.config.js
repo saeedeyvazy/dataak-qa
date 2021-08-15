79497b4a1351f75e69d2836900ca5d87425107d9
saeed eyvazy <saeed.eyvazy@gmail.com>
2021-08-15 22:55:02 +0430

    Add custom text color to tailwindcss config :wrench:

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
 	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		container: {
+			center: true,
+		},
+		extend: {
+			colors: {
+				'qa-black': '#222222',
+			},
+		},
 	},
 	variants: {
 		extend: {},
