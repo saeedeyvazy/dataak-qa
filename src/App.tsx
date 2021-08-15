f71c12546107b5d96400d55356bd87df429d7515
saeed eyvazy <saeed.eyvazy@gmail.com>
2021-08-15 22:57:11 +0430

    Add Header component :construction:

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Switch, Route } from 'react-router'
+import { Home } from './pages'
 
 function App() {
-	return <div className='App'></div>
+	return (
+		<div className='App'>
+			<Router>
+				<Switch>
+					<Route exact path='/' component={Home} />
+				</Switch>
+			</Router>
+		</div>
+	)
 }
 
 export default App
