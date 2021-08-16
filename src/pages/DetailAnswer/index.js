c6f7cfbc47348559659fc8624649b96a1cfb82d4
saeed eyvazy <saeed.eyvazy@gmail.com>
2021-08-16 22:25:50 +0430

    Add textarea to DetailAnswer page :star:

--- src/pages/DetailAnswer/index.js
+++ src/pages/DetailAnswer/index.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import { Header, QuestionCard } from '@components'
+import { Header, QuestionCard, AnswerList, PrimaryButton } from '@components'
 import { PROFILE_IMAGE, ASKER_PROFILE } from '@image-components'
 import { selectedQuestion } from '@features/counter/counterSlice'
 import { useSelector } from 'react-redux'
-import { AnswerList } from '../../components'
 
 function DetailAnswer() {
 	const selectQuestion = useSelector(selectedQuestion)
 	return (
 		<>
-			<div className='space-y-6'>
+			<div className='mb-5 space-y-6'>
 				<Header
 					title='جزییات سوال'
 					username='مرضیه ابراهیمی'
@@ -26,6 +25,20 @@ function DetailAnswer() {
 					/>
 					<span className='text-2xl font-extrabold'>پاسخ ها</span>
 					<AnswerList answerList={selectQuestion.answerList} />
+				</div>
+				<div className='container flex flex-col space-y-6'>
+					<span className='text-2xl font-extrabold'>پاسخ خود را ثبت کنید</span>
+					<span className='text-xs font-bold text-qa-dark-black'>
+						پاسخ خود را بنویسید
+					</span>
+					<textarea
+						placeholder='متن پاسخ...'
+						className='h-40 p-3 border rounded-md outline-none'
+					/>
+					<p className='text-xs text-dislike'>محل قرارگیری متن راهنمای خطا</p>
+				</div>
+				<div className='container'>
+					<PrimaryButton text='ارسال پاسخ' />
 				</div>
 			</div>
 		</>
