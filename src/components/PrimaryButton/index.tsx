68aa425d0e759fe3f7d079bc169e4e06e356a4b2
saeed eyvazy <saeed.eyvazy@gmail.com>
2021-08-16 17:57:28 +0430

    FIX PrimaryButton component font weight :lipstick:

--- src/components/PrimaryButton/index.tsx
+++ src/components/PrimaryButton/index.tsx
@@ -1,6 +1,6 @@
 export const PrimaryButton: React.FC<Props> = ({ text }) => {
 	return (
-		<button className='px-4 py-2 text-xs text-white rounded-md h-9 bg-primary'>
+		<button className='px-4 py-2 text-xs font-bold text-white rounded-md h-9 bg-primary'>
 			{text}
 		</button>
 	)
