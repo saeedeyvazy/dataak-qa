e91c8e9a0270316f2fa994749a7e2bf6f31bdadc
saeed eyvazy <saeed.eyvazy@gmail.com>
2021-08-16 12:33:45 +0430

    make rtl header component :lipstick:

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,13 +6,13 @@ export const Header: React.FC<Props> = ({ title, userImage, username }) => {
 	return (
 		<div className='bg-white'>
 			<div className='container flex items-center justify-between h-16'>
-				<div className='flex items-center gap-4'>
-					<img src={BOTTOM_ARROW} alt='arrow' />
-					<h3 className='text-sm font-bold text-qa-dark-black'>{username}</h3>
-					<img src={userImage} alt='profile' />
-					<PrimaryButton text='سوال جدید' />
-				</div>
 				<h3 className='text-2xl font-extrabold text-qa-black'>{title}</h3>
+				<div className='flex items-center gap-4'>
+					<PrimaryButton text='سوال جدید' />
+					<img src={userImage} alt='profile' />
+					<h3 className='text-sm font-bold text-qa-dark-black'>{username}</h3>
+					<img src={BOTTOM_ARROW} alt='arrow' />
+				</div>
 			</div>
 		</div>
 	)
