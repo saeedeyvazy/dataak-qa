097737ddacbaf2bf3b801113b4b53f3ca7dfd605
saeed eyvazy <saeed.eyvazy@gmail.com>
2021-08-16 10:29:25 +0430

    FIX PrimaryButton component height :lipstick:

--- src/components/PrimaryButton/index.tsx
+++ src/components/PrimaryButton/index.tsx
@@ -1,6 +1,6 @@
 export const PrimaryButton: React.FC<Props> = ({ text }) => {
 	return (
-		<button className='rounded-md bg-primary text-white text-xs px-4 py-2'>
+		<button className='px-4 py-2 text-xs text-white rounded-md h-9 bg-primary'>
 			{text}
 		</button>
 	)
