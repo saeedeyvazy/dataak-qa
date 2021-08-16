a8a83b801ca5d3fed8175ec67439e9ab265dd26b
saeed eyvazy <saeed.eyvazy@gmail.com>
2021-08-16 19:08:25 +0430

    FIX outline of Buttons component :lipstick:

--- src/components/PrimaryButton/index.tsx
+++ src/components/PrimaryButton/index.tsx
@@ -2,7 +2,7 @@ export const PrimaryButton: React.FC<Props> = ({ text, onClick }) => {
 	return (
 		<button
 			onClick={onClick}
-			className='px-4 py-2 text-xs font-bold text-white rounded-md h-9 bg-primary'
+			className='px-4 py-2 text-xs font-bold text-white rounded-md outline-none h-9 bg-primary'
 		>
 			{text}
 		</button>
--- src/components/SecondaryButton/index.tsx
+++ src/components/SecondaryButton/index.tsx
@@ -1,6 +1,6 @@
 export const SecondaryButton: React.FC<Props> = ({ text }) => {
 	return (
-		<button className='px-4 py-2 text-xs font-bold border rounded-md border-primary h-9 text-primary'>
+		<button className='px-4 py-2 text-xs font-bold border rounded-md outline-none border-primary h-9 text-primary'>
 			{text}
 		</button>
 	)
